60d771f112c39d7d024ee2ee0376be1fb1c8eb79
vaishnavigupta12 <vaishnavigupta1073@gmail.com>
2026-06-07 22:51:19 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,38 @@
+#This is my first day of learning python
+#day-1, 30 days of python
+
+#Starting with Basic Math
+print(2+5)
+print(2*5)
+print(87-60)
+print(50/2)
+print(73*24)
+print(2**3)
+print(4**2)
+print(10%3)
+print(10//3)
+
+#Moving to data types
+print(type (10))
+print(type (2.89))
+print(type ("world"))
+print(type (True))
+print(type ([1,2,3,4]))
+name="Vaishnavi"
+print(type (name))
+print(type (1+1j))
+print (type ((1,2)))
+print (type(zip([1,2],[3,4])))
+
+#learnt new types i didn't do during 1st year of uni learning c and c++
+#now, About Me
+
+print ("First Name: Vaishnavi")
+print ("Last Name: Gupta")
+print ("Country: India")
+print ("Age:19")
+
+print(type("Vaishnavi"))
+print (type(19))
+
+#all done for the day, you gotta start somewhere!!
